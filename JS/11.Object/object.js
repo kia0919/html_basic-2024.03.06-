43606f17bbea9188f0305bc9d0d7d84e6bcfd657
kia0919 <john0256@naver.com>
2024-03-21 10:02:09 +0900

    2024.03.21

--- JS/11.Object/object.js
+++ JS/11.Object/object.js
@@ -34,44 +34,44 @@ const goJong = {
 }
 console.log(goJong);
 
-console.log('==============================');
+// console.log('==============================');
 
-/*
-    Object 생성자 : Object 클래스의 생성자로 빈 객체 생성 가능
-*/
-const emptyObjectByConstructor = new Object();
+// /*
+//     Object 생성자 : Object 클래스의 생성자로 빈 객체 생성 가능
+// */
+// const emptyObjectByConstructor = new Object();
 
 
-console.log('==============================');
+// console.log('==============================');
 
-/*
-    생성자 함수 : 생성자 함수를 사용하여 클래스처럼 사용 가능
-*/
-function Person(name, age, address) {
-    this.name = name;
-    this.age = age;
-    this.address = address;
-}
-const goGildong = new Person('고길동', 30, '인천광역시');
-console.log(goGildong);
-console.log(goGildong.name);
+// /*
+//     생성자 함수 : 생성자 함수를 사용하여 클래스처럼 사용 가능
+// */
+// function Person(name, age, address) {
+//     this.name = name;
+//     this.age = age;
+//     this.address = address;
+// }
+// const goGildong = new Person('고길동', 30, '인천광역시');
+// console.log(goGildong);
+// console.log(goGildong.name);
 
-console.log('==============================');
+// console.log('==============================');
 
-/*
-    속성 이름 (속성 키) : 
-    - 일반적으로 문자열로 표기함
-    - 자바스크립트에서 유효한 이름 (변수 이름으로 사용 가능한 이름)일 경우에는 따옴표를 생략할 수 있음
-    - 유효한 이름이 아니면 반드시 따옴표를 포함해서 작성
-*/
-const suhyun = {
-    // 변수명에 사용할 수 있는 이름이 아니면 반드시 다음과 같이 ''따옴표를 붙여줘야 한다.
-    'first name' : 'suhyun',
-    'last-name' : 'su',
-    gender : 'male',
-    age : 32
-};
-console.log(suhyun);
+// /*
+//     속성 이름 (속성 키) : 
+//     - 일반적으로 문자열로 표기함
+//     - 자바스크립트에서 유효한 이름 (변수 이름으로 사용 가능한 이름)일 경우에는 따옴표를 생략할 수 있음
+//     - 유효한 이름이 아니면 반드시 따옴표를 포함해서 작성
+// */
+// const suhyun = {
+//     // 변수명에 사용할 수 있는 이름이 아니면 반드시 다음과 같이 ''따옴표를 붙여줘야 한다.
+//     'first name' : 'suhyun',
+//     'last-name' : 'su',
+//     gender : 'male',
+//     age : 32
+// };
+// console.log(suhyun);
 
 console.log('==============================');
 
