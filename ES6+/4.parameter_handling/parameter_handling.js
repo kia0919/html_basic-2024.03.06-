e144799958136b20f23aa9e3e12687779dc1e8e5
kia0919 <john0256@naver.com>
2024-03-29 11:50:55 +0900

    2024.03.29 basic_ES6+

--- ES6+/4.parameter_handling/parameter_handling.js
+++ ES6+/4.parameter_handling/parameter_handling.js
@@ -4,28 +4,91 @@
 */
 // a, b에 값을 지정핮 않으면 undefined인 a, b를 더했기 때문에 NaN이 나옴
 const add = (a = 10, b = 20) => { return a + b };
-console.log(add()); 
-console.log(add(30, 90));
+console.log(add());     // 30
+console.log(add(30, 90));   // 120
 
 console.log('========================================');
 
 /*
     rest 매개변수 : 
-    - 
-    - 
-    - 
+    - 매개변수 앞에 ... 을 붙여서 정의한 매개변수
+    - 함수에 전달받은 인수의 목록을 배열로 전달 받음
+    - reset 매개변수는 반드시 제일 마지막에 선언되어야 한다.
 */
+const restFunction1 = (...args) => {
+    console.log(args);      //[ 1, 2, 3, 4, 5 ]
+}
+restFunction1(1, 2, 3, 4, 5);
 
-
+const restFunction2 = (arg1, ...args) => {  //rest매개변수는 제일 뒤에다가 배치.
+    console.log(arg1);      //1
+    console.log(args);      // [ 2, 3, 4, 5 ]
+}
+restFunction2(1, 2, 3, 4, 5);
 
 console.log('========================================');
 
 /*
     spread 연산자 : 
-    - 
-    - 
+    - ...을 사용하여 대상을 개별 요소를 분리
+    - 반드시 피연산자는 반복 가능한 객체 (배열, 객체, 문자열 등...)
 */
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);       // [ 1, 2, 3, 4, 5 ]
 
-
+console.log(...numbers);    // 1 2 3 4 5
+console.log(1, 2, 3, 4, 5);
 
 console.log('========================================');
+
+// 배열 복사에 사용
+const copyNumbers = [ ...numbers ];
+console.log(copyNumbers);
+
+copyNumbers[0] = 10;
+console.log(numbers);          // [ 1, 2, 3, 4, 5 ]
+console.log(copyNumbers); // [ 10, 2, 3, 4, 5 ]
+
+console.log('========================================');
+
+// 배열 연결에 사용
+const numbers2 = [6, 7, 8, 9, 10];
+let newNumbers = [...numbers, ...numbers2 ];
+console.log(newNumbers);// [ 1, 2, 3, 4,  5, 6, 7, 8, 9, 10 ]
+newNumbers = [...numbers2, ...numbers];
+console.log(newNumbers); // [ 6, 7, 8, 9, 10, 1, 2, 3, 4,  5]
+
+console.log('========================================');
+
+// 배열 요소 추가에 사용
+newNumbers = [ ...numbers, 0, -1, -2 ];
+console.log(newNumbers);    // [ 1, 2,  3,  4, 5, 0, -1, -2 ]
+
+console.log('========================================');
+
+// 객체 복사, 연결, 요소 추가에 사용
+// 복사
+const king = {
+    name: '이성계',
+    tombName: '태조'
+};
+
+let newKing = { ...king };
+newKing.name = '이방과';
+console.log(newKing);   // { name: '이성계', tombName: '태조' }
+console.log(king);          // { name: '이성계', tombName: '태조' }
+
+// 연결
+const kingInfo = {
+    address: '서울특별시',
+    contry: '고려'
+}
+newKing = { ...king, ...kingInfo };
+console.log(newKing);   // { name: '이성계', tombName: '태조', address: '서울특별시', contry: '고려' }
+
+// 추가
+newKing = { ...king, birth: '1335.11.04' };
+console.log(newKing);   // { name: '이성계', tombName: '태조', birth: '1335.11.04' }
+
+newKing = { ...king, name: '이단', birth: '1335.11.04' };
+console.log(newKing);// { name: '이단', tombName: '태조', birth: '1335.11.04' }
--- ES6+/6.destructuring/destructuring.js
+++ ES6+/6.destructuring/destructuring.js
@@ -1,15 +1,39 @@
 /*
     디스트럭처링 :
-    - 
-    - 
+    - 구조화 된 배열 또는 객체를 비구조화, 구조화 파괴하여 개별 변수로 할당하는 것
+    - 배열 또는 객체에서 필요한 데이터만 추출해서 변수에 할당하여 사용할 수 있도록 함
 */
+
+let king = {
+    name: '이성계',
+    tombname: '태조',
+    birthday: '1335.11.04.',
+    death: '1408.06.27.'
+};
+
+// let name = king.name;
+// let tombName = king.tombName;
+// let birth = king.birth;
+// let death = king.death;
+
+let fruits = ['apple', 'banana', 'melon', 'mango', 'orange'];
+
+// let fruit1 = fruits[0];
+// let fruit2 = fruits[1];
+// let fruit3 = fruits[2];
+// let fruit4 = fruits[3];
+// let fruit5 = fruits[4];
 
 /*
     배열 디스트럭처링
-    - 
-    - 
+    - 배열의 각각의 요소를 배열로부터 추출해서 변수에 할당
+    - 할당 기준은 인덱스
 */
-
+// fruits = ['apple', 'banana', 'melon', 'mango', 'orange'];
+const [fruit1, fruit2, fruit3] = fruits;    
+console.log(fruit1);    // apple
+console.log(fruit2);    // banana
+console.log(fruit3);    // melon
 
 
 console.log('========================================');
