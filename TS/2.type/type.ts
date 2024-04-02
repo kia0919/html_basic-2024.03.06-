9a70481f9394e817043a8ce298f3b2dc3cecd204
kia0919 <john0256@naver.com>
2024-04-02 11:46:41 +0900

    2024.04.02-TS

--- TS/2.type/type.ts
+++ TS/2.type/type.ts
@@ -15,42 +15,73 @@ const arrowFunc = (arg:number): string=> {  //위 코드와 같은 코드
 
 /*
     typescript의 타입
-    - 
-    -
-    - 
-    - 
-    - 
-    - 
-    - 
-    - 
-    - 
-    - 
+    - string: 문자열
+    - number: 숫자
+    - boolean: 논리
+    - null: null값
+    - undefined: 정의되지 않음
+    - object: 참조타입
+    - away: 배열 타입
+    - enum: 열거형 타입
+    - void: 함수에서 반환값이 없음
+    - any: 모든 타입
 */
+// away type
+////배열에 타입을 number로 지정했으므로 숫자만 선언 가능
+const numbers: number[] =[1, 2, 3, 4, 5];   
+// 제너릭 사용하여 타입 선언 가능
+const numbers2: Array<number> = [1, 2, 3, 4, 5];
 
+//enum
+enum Color {
+    RED, GREEN, BLUE
+}
+const color: Color = Color.RED;
+
+// any: 타입 지정을 뭘 해야할 지 모를때 any타입 사용
+// antV변수명에 any타입으로 숫자 10을 선언
+let anyV: any = 10;
+// 모든 타입 접근 가능하여서, 문자열로 접근하여도 가능
+anyV = '10';    
+
+//void
+const aFunc = (): void => {
+
+}
 
 /*
-    타입추론 :
-    - 
+    타입추론 : 자주사용
+    - 타입을 선언하지 않고 변수에 값이 할당되는 순간에 할당된 값에 따라 변수의 타입이 결정나는 것
 */
-
+const stringVariable = '문자열';
+stringVariable = 10;
 
 
 /*
     타입 변환 :
-    - 
+    as: 변수명 변경할 때 사용
+    - as 키워드로 형변환 가능 ( 객체일 때 자주 사용 )
 */
-
-
+const PI: any = '3.14';
+let piNumber: number = PI as number; 
+piNumber = Number(PI);
 
 /*
     유니온 타입
-    - 
+    - 복수의 타입을 지정하는 방법, |
 */
-
-
+let union: number | null;
+union = 10;
+union = null;
+// 지정하지 않은 타입 사용으로 에러 뜸
+union = '10';
 
 /*
     리터럴 타입
-    - 
+    - 지정한 리터럴 값만 가질수 있는 타입
 */
-
+let gender: '남' | '여';
+gender = '남';
+gender = '여';
+// 지정한 리터럴 값이 아니므로 에러 뜸
+gender = '남자';
--- TS/3.class/class.ts
+++ TS/3.class/class.ts
@@ -1,49 +1,99 @@
 /*
     클래스 정의 :
-    - 
+    - 속성을 사전 정의해서 사용
 */
 
+class Sample1 {
+    field: string;
+
+    constructor(field: string) {
+        this.field = field;
+    }
+}
 
 
 console.log('==================================================');
 
 /*
     접근제어자
-    - 
-    - 
-    - 
-    - 
+    - public: 클래스 내부, 자식 클래스 내부, 모든 위치
+    - protected: 클래스 내부, 자식 클래스 내부
+    - private: 클래스 내부
+    - typescript에는 패키지라는 개념이 없기 때문에 기본적으로 public으로 지정됨
 */
-
-
+class Sample2 {
+    private privateField: string;
+    protected protectedField: string;
+    public publicField: String;
+    
+        constructor () {
+            this.privateField = '프라이빗 필드';
+            this.protectedField = '프로텍티트 필드';
+            this.publicField = '퍼블릭 필드'
+        }
+}
 
 console.log('==================================================');
 
 /*
     생성자 매개변수에 접근 제어자 지정
-    - 
+    - 생성자의 매개변수에 접근 제어자를 지정하여 필드 선언 가능
 */
 
+class Sample3 {
 
+    constructor (
+        public publicField: string,
+        protected protectedField: string,
+        private privateField: string,
+        localVariable: string //접근제어자를 지정하지 않아 매개변수로만 지정이 됨
+    ) {
+
+    }
+}
+const sample3Instance = new Sample3('publicField', 'protectedField', 'privateField', 'localVariable');
+console.log(sample3Instance);
 
 console.log('==================================================');
 
 /*
     readonly :
-    -  
-    - 
-    - 
+    -  readonly가 선언된 클래스 속성 선언 시 또는 생성자 내부에서만 값을 할당 할 수 있음
+    - 값을 재할당 할 수 없고 오직 읽기만 가능
+    - 상수 선언에 사용됨
 */
 
+class Sample4 {
+    
+    public readonly field1: string = '퍼블릭 읽기 전용 필드1'
+    public readonly field2: string;
+
+    constructor (
+        field2: string,
+        public readonly field3: string
+    ){
+        this.field2 = field2;
+    }
+}
+const sample4Instance = new Sample4('퍼블릭 읽기전용 필드2', '퍼블릭 읽기전용 필드3');
+console.log(sample4Instance);
+// sample4Instance.field1 = '변경'    읽기 전용이기에 값을 변경 못함
 
 
 console.log('==================================================');
 
 /*
     추상 클래스 :
-    - 
+    - 하나 이상의 추상 메서드를 포함하는 클래스, abstaract키워드로 선언
 */
+abstract class AbstractClass {
+    abstract abstractMethod(arg: string): string; //반환타입이 string
+}
 
-
+class SubClass extends AbstractClass {
+    abstractMethod(arg: string): string {
+        return arg;
+    }
+}
 
 console.log('==================================================');
